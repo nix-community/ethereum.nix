66b6faa97d0d2ea5090bf42167debea16e16ee55
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2022-09-10 20:33:15 +0200

    feat: prysm update to v3.1.1 and removal of unnecessary binaries

--- packages/clients/consensus/prysm/default.nix
+++ packages/clients/consensus/prysm/default.nix
@@ -4,21 +4,34 @@
   buildGoModule,
   fetchFromGitHub,
   libelf,
+  lib,
 }:
 buildGoModule rec {
   pname = "prysm";
-  version = "3.0.0";
+  version = "3.1.1";
 
   src = fetchFromGitHub {
     owner = "prysmaticlabs";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-316bJy4outYfCs70bKL9wwQ/YTBZLt45CusBsI1lFxE=";
+    hash = "sha256-iy+UayBKEwcgmJT2uuO1q9vmrABdeXzEMTxEVbUGQiA=";
   };
 
-  vendorSha256 = "sha256-Tv59UG3i9Wpv9Vf012oC2EKgtYj5FiWaySdU3qNeYpw=";
+  vendorSha256 = "sha256-gy/8tHuDl5win03pE/+61um7EU3pQEj1Xim94LwK5uE=";
 
   buildInputs = [bls blst libelf];
 
+  subPackages = [
+    "cmd/beacon-chain"
+    "cmd/client-stats"
+    "cmd/prysmctl"
+    "cmd/validator"
+  ];
+
   doCheck = false;
+
+  meta = {
+    description = "Go implementation of Ethereum proof of stake";
+    homepage = "https://github.com/prysmaticlabs/prysm";
+  };
 }
