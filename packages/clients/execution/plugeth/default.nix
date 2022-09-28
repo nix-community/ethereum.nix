987ec03ccd3cf2baf51ad7cbe08adcd97641f259
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2022-09-28 15:03:27 +0000

    feat: bump plugeth version to 1.10.25.0.0

--- packages/clients/execution/plugeth/default.nix
+++ packages/clients/execution/plugeth/default.nix
@@ -1,11 +1,11 @@
 {mkGeth}:
 mkGeth {
   name = "plugeth";
-  version = "1.10.18.0.0";
+  version = "1.10.25.0.0";
   owner = "openrelayxyz";
   repo = "plugeth";
-  sha256 = "sha256-FhQe3WIzc35zgtE10cHpif2f7bJcz/rYo9SzT5FCZqk=";
-  vendorSha256 = "sha256-JtBWNMbRtJnwTVcwBXwmKV+6OeghFxs2GQVJike5BbQ=";
+  sha256 = "sha256-sibt+rud2eNskC4TXbWUOCmvzpwUEdTSi/WQiu4Mwpc=";
+  vendorSha256 = "sha256-xP4jbaQUB3VvyD8sktfFpTDUZVEKtskqkVb/GkTYp54=";
   bins = ["geth"];
   subPackages = ["cmd/geth"];
 }
