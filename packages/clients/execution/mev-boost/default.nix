68c05f08880323214b54fd7b8a0912fca4855e42
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2022-09-28 14:49:46 +0000

    feat: bump mev-boost version to 1.3.2

--- packages/clients/execution/mev-boost/default.nix
+++ packages/clients/execution/mev-boost/default.nix
@@ -4,11 +4,11 @@
 }:
 mkGeth {
   name = "mev-boost";
-  version = "0.8.2";
+  version = "1.3.2";
   owner = "flashbots";
   repo = "mev-boost";
-  sha256 = "sha256-Cx5BL8ZR54MsuAvfVgUpC4+VMDS6gLUGgRa+sT+x3nw=";
-  vendorSha256 = "sha256-HKp3zCOOiRmn25cSKlAo6/S/bqKFBmMzRbyDDwdQkzc=";
+  sha256 = "sha256-tqiu5wJsgwdFJH1gvbCqgCstMMWA/As7tSNBwv9rMdA=";
+  vendorSha256 = "sha256-qKlltwacgCNfuMcnkCwhOZtIRqDJTarOd1Vf9C6oIk4=";
   subPackages = ["cmd/mev-boost"];
   buildInputs = [blst];
   bins = [];
