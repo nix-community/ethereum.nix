38fd0a6da38aec2926b62d608e02d144f59c1bcb
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2022-09-28 15:00:22 +0000

    feat: bump mev-geth version to 1.10.23-mev0.7.0

--- packages/clients/execution/mev-geth/default.nix
+++ packages/clients/execution/mev-geth/default.nix
@@ -1,10 +1,10 @@
 {mkGeth}:
 mkGeth {
   name = "mev-geth";
-  version = "1.10.19-mev0.6.1";
+  version = "1.10.23-mev0.7.0";
   owner = "flashbots";
   repo = "mev-geth";
-  sha256 = "sha256-8czmwlbUHx2yR4qBlmABO/ywXGUZYYmL5wXlhxUxAqk=";
-  vendorSha256 = "sha256-yOR/XLY54R2w5Dz/xDZ9mDzvYOobUxuxu355jqVPm2k=";
+  sha256 = "sha256-DR9Tg/oMTRcGEccsCc1M4D2pffuM0/qGmtHeaKkXXVQ=";
+  vendorSha256 = "sha256-Dj+xN8lr98LJyYr2FwJ7yUIJkUeUrr1fkcbj4hShJI0=";
   bins = ["geth"];
 }
