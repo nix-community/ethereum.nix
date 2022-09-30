0ff0bb4f06ccdb87b33b2982d28a9ee94dfa0313
Brian McGee <brian@41north.dev>
2022-09-30 11:14:25 +0100

    feat: remove cachix substituters

--- flake.nix
+++ flake.nix
@@ -5,14 +5,10 @@
     substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
-      "https://pre-commit-hooks.cachix.org"
-      "https://ethereum-nix.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
-      "ethereum-nix.cachix.org-1:mpmQuO1myAs3CXDBLh8uQy4QDFtemaDKLD4UKmVjByE="
     ];
   };
 
