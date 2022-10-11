1e5898b0ade8ba89d6c0f8e65e377502bdfbbd68
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2022-10-11 11:58:41 +0000

    feat: update substituters to use extra-substituters

--- flake.nix
+++ flake.nix
@@ -2,13 +2,15 @@
   description = "A reproducible Nix package set for Ethereum clients and utilities";
 
   nixConfig = {
-    substituters = [
+    extra-substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
+      "https://cache.41n.dev"
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.41n.dev-1:myz16wqUMGB6cnefRtBzNMpeusqsiZYEcaadKLJnamE="
     ];
   };
 
