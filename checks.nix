dba8cc1fc0d8b03551272bf21c74f29b88c53330
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2022-11-29 16:55:20 +0000

    fix: removal pre-commit-hooks and add nix-linter check

--- checks.nix
+++ checks.nix
@@ -1,16 +1,21 @@
 {
   inputs,
   packages,
+  pkgs,
+  self,
   system,
 }:
 {
-  pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-    src = ./.;
-    hooks = {
-      alejandra.enable = true;
-      nix-linter.enable = true;
-    };
-  };
+  nix-lint =
+    pkgs.runCommand "nix-linter" {
+      nativeBuildInputs = with pkgs; [nix-linter];
+    } ''
+      # keep timestamps so that treefmt is able to detect mtime changes
+      cp --no-preserve=mode --preserve=timestamps -r ${self} source
+      cd source
+      HOME=$TMPDIR nix-linter *.nix
+      touch $out
+    '';
 }
 # nix build support only pointing to a single derivation
 # by including packages we can build all of them in one go
