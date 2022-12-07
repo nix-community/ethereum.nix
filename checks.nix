eac52b37348c8973953a5b7ee9fe9ea56e48b7a2
Aldo <82811+aldoborrero@users.noreply.github.com>
2022-12-07 13:55:58 +0000

    fix: solve bad configuration of nix-linter (#28)

--- checks.nix
+++ checks.nix
@@ -9,8 +9,10 @@
       nativeBuildInputs = with pkgs; [nix-linter];
     } ''
       cp --no-preserve=mode -r ${self} source
+      rm -rf source/.direnv # delete .direnv cache so nix-linter don't recurse it
       cd source
-      HOME=$TMPDIR nix-linter *.nix
+      HOME=$TMPDIR nix-linter --recursive
+      touch $out
     '';
 }
 # nix build support only pointing to a single derivation
