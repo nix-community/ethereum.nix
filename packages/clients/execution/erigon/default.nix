0a12b354d6deb0f643997f742ac5856c2ea80fdf
Brian McGee <brian@41north.dev>
2022-12-16 08:21:11 +0000

    chore(erigon): 2.30.0 -> 2.32.0

--- packages/clients/execution/erigon/default.nix
+++ packages/clients/execution/erigon/default.nix
@@ -5,17 +5,17 @@
 }:
 buildGoModule rec {
   pname = "erigon";
-  version = "2.30.0";
+  version = "2.32.0";
 
   src = fetchFromGitHub {
     owner = "ledgerwatch";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-s5D+Ps5S95AyNh7tt2SnTvUxNHzOothsMZA7NW3x0yM=";
+    sha256 = "sha256-1o9FwvCXDxzEU3dIM9oiJAu6ZRx1qP15OgBZQkv5fvA=";
     fetchSubmodules = true;
   };
 
-  vendorSha256 = "sha256-ICoThps7c4+9NQPeaASQ88YVbczJD/MgF2ldOmKjgvc=";
+  vendorSha256 = "sha256-YCTfFuqzSlC3iLIyD59lpunKytBbNs6n8cxmmypCSzI=";
   proxyVendor = true;
 
   # Build errors in mdbx when format hardening is enabled:
