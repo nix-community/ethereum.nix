a46966f5a13942e1757e28b552af7ed37ca78682
BMG <brian@41north.dev>
2023-01-04 16:02:10 +0000

    fix: disable nix-linter (#56)

--- checks.nix
+++ checks.nix
@@ -2,14 +2,14 @@
   pkgs,
   self,
 }: {
-  nix-lint =
-    pkgs.runCommand "nix-linter" {
-      nativeBuildInputs = with pkgs; [nix-linter];
-    } ''
-      cp --no-preserve=mode -r ${self} source
-      rm -rf source/.direnv # delete .direnv cache so nix-linter don't recurse it
-      cd source
-      HOME=$TMPDIR nix-linter --recursive
-      touch $out
-    '';
+  #  nix-lint =
+  #    pkgs.runCommand "nix-linter" {
+  #      nativeBuildInputs = with pkgs; [nix-linter];
+  #    } ''
+  #      cp --no-preserve=mode -r ${self} source
+  #      rm -rf source/.direnv # delete .direnv cache so nix-linter don't recurse it
+  #      cd source
+  #      HOME=$TMPDIR nix-linter --recursive
+  #      touch $out
+  #    '';
 }
