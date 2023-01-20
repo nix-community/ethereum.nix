5b12cc59a0970ed0521ca4f258c192109e94b66b
Brian McGee <brian@41north.dev>
2023-01-20 10:09:19 +0000

    feat: add packages to flake checks

--- nix/checks.nix
+++ nix/checks.nix
@@ -1,21 +1,34 @@
-{self, ...}: {
-  perSystem = {
+args @ {
+  self,
+  inputs,
+  lib,
+  ...
+}: let
+  packagesModule = import ../packages args;
+in {
+  perSystem = psArgs @ {
     self',
     pkgs,
     ...
-  }: {
-    checks = {
-      # TODO: Switch to statix instead
-      # nix-lint =
-      #   pkgs.runCommand "nix-linter" {
-      #     nativeBuildInputs = with pkgs; [nix-linter];
-      #   } ''
-      #     cp --no-preserve=mode -r ${self} source
-      #     rm -rf source/.direnv # delete .direnv cache so nix-linter don't recurse it
-      #     cd source
-      #     HOME=$TMPDIR nix-linter --recursive
-      #     touch $out
-      #   '';
-    };
+  }: let
+    # load package derivations
+    inherit (packagesModule.perSystem psArgs) packages;
+  in {
+    checks =
+      {
+        # TODO: Switch to statix instead
+        # nix-lint =
+        #   pkgs.runCommand "nix-linter" {
+        #     nativeBuildInputs = with pkgs; [nix-linter];
+        #   } ''
+        #     cp --no-preserve=mode -r ${self} source
+        #     rm -rf source/.direnv # delete .direnv cache so nix-linter don't recurse it
+        #     cd source
+        #     HOME=$TMPDIR nix-linter --recursive
+        #     touch $out
+        #   '';
+      }
+      # merge in the package derivations to force a build of all packages during a `nix flake check`
+      // packages;
   };
 }
