80f72bd19168a524ddc3ac9f91ab51fd56d479ec
Brian McGee <brian@bmcgee.ie>
2023-02-09 10:34:56 +0000

    feat(modules/backup): add support for in-situ backups of non-btrfs state directories

--- modules/backup.nix
+++ modules/backup.nix
@@ -74,8 +74,60 @@
     SERVICE_NAME=$(basename $STATE_DIRECTORY)
     VOLUME_DIR=/var/lib/private/$SERVICE_NAME
 
-    # ensure cowdata is disabled
-    chattr +C $VOLUME_DIR
+    if ! btrfs sub show $VOLUME_DIR > /dev/null; then
+        echo "$VOLUME_DIR is not a btrfs subvolume, exiting"
+        exit 0
+    fi
+
+    echo "Disabling copy on write"
+    chattr -R +C $VOLUME_DIR
+  '';
+
+  chainHeightScript = pkgs.writeShellScript "chain-height" ''
+    set -euo pipefail
+
+    # metadata path
+    METADATA_JSON=$1
+
+    # check if the metadata file exists
+    if [ ! -f $METADATA_JSON ]; then
+        >&2 echo "Could not locate $METADATA_JSON, skipping snapshot"
+        # client must have been in a bad state or it had not been able to initialise yet to a point
+        # where it's RPC was active and returning so we indicate an error code
+        exit 1
+    fi
+
+    # ensure the metadata file is recent
+    NOW_SECONDS=$(date +%s)
+    METADATA_SECONDS=$(date +%s -r $METADATA_JSON)
+    METADATA_AGE=$((NOW_SECONDS - METADATA_SECONDS))
+
+    if [ $METADATA_AGE -gt 30 ]; then
+        >&2 echo "$METADATA_JSON is older than 30 seconds, skipping snapshot"
+        # suspected failure in the metadata service, we can't be sure about the latest state of the client
+        exit 1
+    fi
+
+    # determine the chain height from the metadata
+    HEIGHT=$(cat $METADATA_JSON | jq .height)
+
+    if [ -z $\{HEIGHT+x} ]; then
+        >&2 echo "Could not determine height from $METADATA_JSON, skipping snapshot";
+        # metadata is malformed
+        exit 1
+    fi
+  '';
+
+  # It's necessary to record the exit status so that we can safeguard against backing up unclean state directories
+  # in the case of performing an in-situ backup. For btrfs snapshot based backups, the snapshot script already
+  # performs an exit status check before snapshotting.
+
+  clearExitStatusScript = pkgs.writeShellScript "clear-exit-status" ''
+    rm -r $STATE_DIRECTORY/.exit-status
+  '';
+
+  recordExitStatusScript = pkgs.writeShellScript "record-exit-status" ''
+    echo $EXIT_STATUS > $STATE_DIRECTORY/.exit-status
   '';
 
   snapshotVolumeScript = pkgs.writeShellScript "snapshot-volume" ''
@@ -91,30 +143,19 @@
     SERVICE_NAME=$(basename $STATE_DIRECTORY)
     VOLUME_DIR=/var/lib/private/$SERVICE_NAME
 
-    # metadata path
+    # check if the volume is in fact a btrfs subvolume
+    if ! btrfs sub show $VOLUME_DIR > /dev/null; then
+        echo "$VOLUME_DIR is not a btrfs subvolume, skipping snapshot"
+        exit 0  # clean exit as this hook will be registered against every service
+    fi
+
+    # determine chain height from metadata
     METADATA_JSON="$STATE_DIRECTORY/.metadata.json"
-
-    # check if the metadata file exists
-    if [ ! -f $METADATA_JSON ]; then
-        echo "Could not locate $METADATA_JSON, skipping snapshot"
-        exit 1
-    fi
-
-    # ensure the metadata file is recent
-    NOW_SECONDS=$(date +%s)
-    METADATA_SECONDS=$(date +%s -r $METADATA_JSON)
-    METADATA_AGE=$((NOW_SECONDS - METADATA_SECONDS))
-
-    if [ $METADATA_AGE -gt 30 ]; then
-        echo "$METADATA_JSON is older than 30 seconds, skipping snapshot"
-        exit 1
-    fi
-
-    # determine the chain height from the metadata
-    HEIGHT=$(cat $METADATA_JSON | jq .height)
+    HEIGHT=$(${chainHeightScript} $METADATA_JSON)
 
     if [ -z $\{HEIGHT+x} ]; then
-        echo "Could not determine height from $METADATA_JSON, skipping snapshot";
+        >&2 echo "Could not determine height from $METADATA_JSON, skipping snapshot";
+        # metadata is malformed
         exit 1
     fi
 
@@ -165,7 +206,7 @@
       };
     };
 
-  addSnapshotToService = name:
+  instrumentClientService = name:
     nameValuePair "${name}" {
       path = with pkgs; [
         btrfs-progs
@@ -174,9 +215,11 @@
       ];
       serviceConfig = {
         ExecStartPre = mkBefore [
+          clearExitStatusScript
           "+${setupVolumeScript}"
         ];
         ExecStopPost = mkAfter [
+          recordExitStatusScript
           "+${snapshotVolumeScript}"
         ];
       };
@@ -185,14 +228,15 @@
   backupScript = pkgs.writeShellScript "backup" ''
     set -euo pipefail
 
-    echo "Running backup for the following services: $@"
-
     export BORG_RSH="ssh -o StrictHostKeyChecking=no -i $CREDENTIALS_DIRECTORY/sshKey";
 
     SERVICE_NAMES=$@
+    echo "Running backup for the following services: $SERVICE_NAMES"
 
-    for SERVICE in $SERVICE_NAMES; do
-        REPO="${cfg.borg.repo}/$SERVICE"
+    # first we ensure there is a borg repo for each configured service
+
+    for SERVICE_NAME in $SERVICE_NAMES; do
+        REPO="${cfg.borg.repo}/$SERVICE_NAME"
 
         if ! borg list $REPO > /dev/null; then
             echo "Creating repo: $REPO"
@@ -200,32 +244,81 @@
         fi
     done
 
-    for SERVICE in $SERVICE_NAMES; do
+    # next we go on a per-service basis:
+    #
+    #   - if the state directory is a btrfs subvolume, we restart the service to generate the snapshot and then backup
+    #   - if the state directory is not a btrfs subvolume, we stop the service, perform the backup and then start the
+    #     service again
 
-        REPO="${cfg.borg.repo}/$SERVICE"
+    for SERVICE_NAME in $SERVICE_NAMES; do
 
-        # restart the service to create the snapshot
-        echo "Restarting $SERVICE"
-        systemctl restart "$SERVICE.service"
+        REPO="${cfg.borg.repo}/$SERVICE_NAME"
+        STATE_DIRECTORY=/var/lib/private/$SERVICE_NAME
 
-        echo "Backing up snapshots for $SERVICE"
+        if btrfs sub show $STATE_DIRECTORY > /dev/null; then
 
-        # reverse order ensures the greatest chain height first and reduces the bandwidth needed
-        # to transfer earlier archives
-        ARCHIVES=$(ls -r "$SNAPSHOT_DIR/$SERVICE")
+            echo "$STATE_DIRECTORY is a btrfs subvolume"
 
-        for ARCHIVE in $ARCHIVES; do
-            if borg list $REPO::$ARCHIVE > /dev/null; then
-                echo "Archive $REPO::$ARCHIVE already exists, skipping"
-            else
-                borg create -s --verbose \
-                    --lock-wait ${builtins.toString cfg.borg.lockWait} \
-                    --compression ${cfg.borg.compression} \
-                    --exclude ${excludeFile} \
-                    $REPO::$ARCHIVE \
-                    $SNAPSHOT_DIR/$SERVICE/$ARCHIVE
+            # restart the service to create the snapshot
+            echo "Restarting $SERVICE_NAME"
+            systemctl restart "$SERVICE_NAME.service"
+
+            echo "Backing up snapshots for $SERVICE_NAME"
+
+            # reverse order ensures the greatest chain height first and reduces the bandwidth needed
+            # to transfer earlier archives
+            ARCHIVES=$(ls -r "$SNAPSHOT_DIR/$SERVICE_NAME")
+
+            for ARCHIVE in $ARCHIVES; do
+                if borg list $REPO::$ARCHIVE > /dev/null; then
+                    echo "Archive $REPO::$ARCHIVE already exists, skipping"
+                else
+                    borg create -s --verbose \
+                        --lock-wait ${builtins.toString cfg.borg.lockWait} \
+                        --compression ${cfg.borg.compression} \
+                        --exclude ${excludeFile} \
+                        $REPO::$ARCHIVE \
+                        $SNAPSHOT_DIR/$SERVICE_NAME/$ARCHIVE
+                fi
+            done
+
+        else
+            echo "$STATE_DIRECTORY is not a btrfs subvolume, performing in-situ backup"
+
+            # stop the service
+            echo "Stopping $SERVICE_NAME"
+            systemctl stop "$SERVICE_NAME.service"
+
+            # check that the process stopped cleanly
+            EXIT_STATUS=$(cat $STATE_DIRECTORY/.exit-status)
+
+            if [ $EXIT_STATUS -ne 0 ];
+                >&2 echo "Unclean shutdown detected, exit status: $EXIT_STATUS. Skipping backup"
+            then
+                # determine chain height from metadata
+                METADATA_JSON="$STATE_DIRECTORY/.metadata.json"
+                HEIGHT=$(${chainHeightScript} $METADATA_JSON)
+
+                if [ -z $\{HEIGHT+x} ]; then
+                    >&2 echo "Could not determine height from $METADATA_JSON, skipping backup";
+                else
+                    ARCHIVE=$HEIGHT
+                    if borg list $REPO::$HEIGHT > /dev/null; then
+                        echo "Archive $REPO::$ARCHIVE already exists, skipping"
+                    else
+                        borg create -s --verbose \
+                            --lock-wait ${builtins.toString cfg.borg.lockWait} \
+                            --compression ${cfg.borg.compression} \
+                            --exclude ${excludeFile} \
+                            $REPO::$ARCHIVE \
+                            $STATE_DIRECTORY
+                    fi
+                fi
             fi
-        done
+
+            # start the service again
+            systemctl start "$SERVICE_NAME.service"
+        fi
     done
   '';
 
@@ -233,6 +326,8 @@
     description = "Backup service for Ethereum clients";
     path = with pkgs; [
       borgbackup
+      btrfs-progs
+      jq
       openssh
       systemd
     ];
@@ -341,7 +436,7 @@ in {
   config.systemd.services = listToAttrs (
     [backupService]
     ++ ((builtins.map mkMetadataService) cfg.services)
-    ++ ((builtins.map addSnapshotToService) cfg.services)
+    ++ ((builtins.map instrumentClientService) cfg.services)
   );
 
   config.systemd.timers = listToAttrs (
