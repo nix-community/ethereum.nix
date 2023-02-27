nix-community/ethereum.nix

commit 08b62f29e9a84606ec8dfcdda43098da273ee41e

feat/multi system (#114)

nix/lib/flake.nix
@@ -1,0 +1,38 @@
+lib: rec {
+  platformPkgs = system:
+    with lib;
+      filterAttrs
+      (_: value: let
+        platforms = attrByPath ["meta" "platforms"] [] value;
+      in
+        elem system platforms);
+
+  buildApps = packages: apps:
+    with lib;
+      listToAttrs
+      (collect (attrs: builtins.attrNames attrs == ["name" "value"])
+        (mapAttrsRecursiveCond builtins.isAttrs (path: _: let
+          drvName = head path;
+          drv = packages.${drvName};
+          name = last (init path);
+        in
+          nameValuePair name {inherit drv name;})
+        apps));
+
+  platformApps = packages: apps:
+    with lib; let
+      apps' = filterAttrs (name: _: elem name (attrNames packages)) apps;
+      bapps = buildApps packages apps';
+    in
+      mapAttrs (_: mkApp) bapps;
+
+  # Taken from flake-utils: https://github.com/numtide/flake-utils/blob/5aed5285a952e0b949eb3ba02c12fa4fcfef535f/default.nix#L195
+  mkApp = {
+    drv,
+    name ? drv.pname or drv.name,
+    exePath ? drv.passthru.exePath or "/bin/${name}",
+  }: {
+    type = "app";
+    program = "${drv}${exePath}";
+  };
+}

flake.nix
@@ -46,7 +46,8 @@
     nixpkgs,
     ...
   }: let
-    lib = import ./nix/lib {inherit (nixpkgs) lib;} // nixpkgs.lib;
+    lib = nixpkgs.lib.extend (final: _: import ./nix/lib final);
+    #    lib = import ./nix/lib {inherit (nixpkgs) lib;} // nixpkgs.lib;
   in
     (flake-parts.lib.evalFlakeModule
       {
@@ -64,8 +65,14 @@
           ./nix
           ./packages
           ./modules
+          ./tests
         ];
-        systems = ["x86_64-linux"];
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ];
       })
     .config
     .flake;

nix/checks.nix
@@ -9,13 +9,9 @@ in {
   perSystem = psArgs @ {
     self',
     pkgs,
+    config,
     ...
-  }: let
-    # load package derivations
-    inherit (packagesModule.perSystem psArgs) packages;
-    # import integration tests
-    integrationTests = import ./../tests {inherit self' inputs pkgs;};
-  in {
+  }: {
     checks =
       {
         statix =
@@ -29,8 +25,8 @@ in {
           '';
       }
       # add integration tests for our custom nixosModules
-      // integrationTests
+      // config.tests
       # merge in the package derivations to force a build of all packages during a `nix flake check`
-      // packages;
+      // self'.packages;
   };
 }

nix/lib/default.nix
@@ -1,10 +1,5 @@
-{lib, ...} @ args: let
-  mkLib = self: let
-    importLib = file: import file ({inherit self;} // args);
-  in {
-    flakes = importLib ./flakes.nix;
-
-    inherit (self.flakes) mkApp;
-  };
-in
-  lib.makeExtensible mkLib
+lib: let
+  flake = import ./flake.nix lib;
+in {
+  inherit flake;
+}

nix/lib/flakes.nix
@@ -1,11 +1,0 @@
-_: {
-  # Taken from flake-utils: https://github.com/numtide/flake-utils/blob/5aed5285a952e0b949eb3ba02c12fa4fcfef535f/default.nix#L195
-  mkApp = {
-    drv,
-    name ? drv.pname or drv.name,
-    exePath ? drv.passthru.exePath or "/bin/${name}",
-  }: {
-    type = "app";
-    program = "${drv}${exePath}";
-  };
-}

packages/clients/consensus/lighthouse/default.nix
@@ -87,5 +87,6 @@ in
     meta = {
       description = "Ethereum consensus client in Rust";
       homepage = "https://github.com/sigp/lighthouse";
+      platforms = ["x86_64-linux"];
     };
   }

packages/clients/consensus/prysm/default.nix
@@ -32,5 +32,6 @@ buildGoModule rec {
   meta = {
     description = "Go implementation of Ethereum proof of stake";
     homepage = "https://github.com/prysmaticlabs/prysm";
+    platforms = ["x86_64-linux"];
   };
 }

packages/clients/consensus/teku/default.nix
@@ -29,5 +29,6 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/ConsenSys/teku";
     license = licenses.asl20;
     sourceProvenance = with sourceTypes; [binaryBytecode];
+    platforms = ["x86_64-linux"];
   };
 }

commit 809cfda3c146db1c928cb62ccb56ceaeddc9d740

fix(apps): geth and prysm apps were broken by #114

nix/lib/flake.nix
@@ -11,12 +11,13 @@ lib: rec {
     with lib;
       listToAttrs
       (collect (attrs: builtins.attrNames attrs == ["name" "value"])
-        (mapAttrsRecursiveCond builtins.isAttrs (path: _: let
+        (mapAttrsRecursiveCond builtins.isAttrs (path: v: let
           drvName = head path;
           drv = packages.${drvName};
           name = last (init path);
+          exePath = "/bin/${v}";
         in
-          nameValuePair name {inherit drv name;})
+          nameValuePair name {inherit drv name exePath;})
         apps));
 
   platformApps = packages: apps: