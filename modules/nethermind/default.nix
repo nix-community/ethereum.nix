657420c4aa5a9c1225cced46a6226b7581d5a376
Brian McGee <brian@bmcgee.ie>
2023-03-08 15:18:43 +0000

    feat(modules/nethermind): separate options types from the main module

--- modules/nethermind/default.nix
+++ modules/nethermind/default.nix
@@ -41,175 +41,9 @@
 
   # capture config for all configured netherminds
   eachNethermind = config.services.ethereum.nethermind;
-
-  # submodule options
-  nethermindOpts = {
-    options = {
-      enable = mkEnableOption (mdDoc "Nethermind Ethereum Node.");
-
-      package = mkOption {
-        type = types.package;
-        default = pkgs.nethermind;
-        defaultText = literalExpression "pkgs.nethermind";
-        description = mdDoc "Package to use as Nethermind.";
-      };
-
-      args = {
-        baseDbPath = mkOption {
-          type = types.nullOr types.path;
-          default = null;
-          description = mdDoc "Configures the path of the Nethermind's database folder.";
-        };
-
-        config = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          example = "mainnet";
-          description = mdDoc "Determines the configuration file of the network on which Nethermind will be running.";
-        };
-
-        configsDirectory = mkOption {
-          type = types.nullOr types.path;
-          default = null;
-          description = mdDoc "Changes the source directory of your configuration files.";
-        };
-
-        log = mkOption {
-          type = types.enum [
-            "OFF"
-            "TRACE"
-            "DEBUG"
-            "INFO"
-            "WARN"
-            "ERROR"
-          ];
-          default = "INFO";
-          description = mdDoc "Changes the logging level.";
-        };
-
-        loggerConfigSource = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = mdDoc "Changes the path of the NLog.config file.";
-        };
-
-        modules = {
-          # https://docs.nethermind.io/nethermind/ethereum-client/configuration/network
-          Network = {
-            DiscoveryPort = mkOption {
-              type = types.port;
-              default = 30303;
-              description = mdDoc "UDP port number for incoming discovery connections.";
-            };
-
-            P2PPort = mkOption {
-              type = types.port;
-              default = 30303;
-              description = mdDoc "TPC/IP port number for incoming P2P connections.";
-            };
-          };
-
-          # https://docs.nethermind.io/nethermind/ethereum-client/configuration/jsonrpc
-          JsonRpc = {
-            Enabled = mkOption {
-              type = types.bool;
-              default = true;
-              description = mdDoc "Defines whether the JSON RPC service is enabled on node startup.";
-            };
-
-            Port = mkOption {
-              type = types.port;
-              default = 8545;
-              description = mdDoc "Port number for JSON RPC calls.";
-            };
-
-            WebSocketsPort = mkOption {
-              type = types.port;
-              default = 8545;
-              description = mdDoc "Port number for JSON RPC web sockets calls.";
-            };
-
-            EngineHost = mkOption {
-              type = types.str;
-              default = "127.0.0.1";
-              description = mdDoc "Host for JSON RPC calls.";
-            };
-
-            EnginePort = mkOption {
-              type = types.port;
-              default = 8551;
-              description = mdDoc "Port for Execution Engine calls.";
-            };
-
-            JwtSecretFile = mkOption {
-              type = types.nullOr types.str;
-              default = null;
-              description = mdDoc "Path to file with hex encoded secret for jwt authentication.";
-              example = "/var/run/geth/jwtsecret";
-            };
-          };
-
-          # https://docs.nethermind.io/nethermind/ethereum-client/configuration/healthchecks
-          HealthChecks = {
-            Enabled = mkOption {
-              type = types.bool;
-              default = true;
-              description = mdDoc "If 'true' then Health Check endpoints is enabled at /health.";
-            };
-          };
-
-          # https://docs.nethermind.io/nethermind/ethereum-client/configuration/metrics
-          Metrics = {
-            Enabled = mkOption {
-              type = types.bool;
-              default = true;
-              description = mdDoc "If 'true',the node publishes various metrics to Prometheus Pushgateway at given interval.";
-            };
-
-            ExposePort = mkOption {
-              type = types.nullOr types.port;
-              default = null;
-              description = mdDoc "If 'true' then Health Check endpoints is enabled at /health";
-            };
-          };
-        };
-      };
-
-      extraArgs = mkOption {
-        type = types.listOf types.str;
-        description = mdDoc "Additional arguments to pass to Nethermind.";
-        default = [];
-      };
-
-      openFirewall = mkOption {
-        type = types.bool;
-        default = false;
-        description = lib.mdDoc "Open ports in the firewall for any enabled networking services";
-      };
-
-      # mixin backup options
-      backup = let
-        inherit (import ../backup/lib.nix lib) options;
-      in
-        options;
-
-      # mixin restore options
-      restore = let
-        inherit (import ../restore/lib.nix lib) options;
-      in
-        options;
-    };
-  };
 in {
   ###### interface
-
-  options = {
-    services.ethereum.nethermind = mkOption {
-      type = types.attrsOf (types.submodule nethermindOpts);
-      default = {};
-      description = mdDoc "Specification of one or more Nethermind instances.";
-    };
-  };
+  inherit (import ./options.nix {inherit lib pkgs;}) options;
 
   ###### implementation
 
@@ -271,11 +105,13 @@ in {
                 else "";
 
               # generate flags
-              args = mkArgs {
-                inherit pathReducer argReducer argFormatter;
-                inherit (cfg) args;
-                opts = nethermindOpts.options.args;
-              };
+              args = let
+                opts = import ./args.nix lib;
+              in
+                mkArgs {
+                  inherit pathReducer argReducer argFormatter opts;
+                  inherit (cfg) args;
+                };
 
               # filter out certain args which need to be treated differently
               specialArgs = ["--JsonRpc.JwtSecretFile"];
