0c6c5655a3e452e98a2d71971cff79c35d64d3cd
Brian McGee <brian@bmcgee.ie>
2023-03-08 15:22:55 +0000

    feat(modules/prysm-beacon): separate options types from the main module

---
+++ modules/prysm-beacon/args.nix
@@ -0,0 +1,107 @@
+lib:
+with lib; {
+  network = mkOption {
+    type = types.nullOr (types.enum ["goerli" "prater" "ropsten" "sepolia"]);
+    default = null;
+    description = mdDoc "The network to connect to. Mainnet (null) is the default ethereum network.";
+  };
+
+  jwt-secret = mkOption {
+    type = types.str;
+    default = null;
+    description = mdDoc "Path to a file containing a hex-encoded string representing a 32 byte secret used for authentication with an execution node via HTTP";
+    example = "/var/run/prysm/jwtsecret";
+  };
+
+  checkpoint-sync-url = mkOption {
+    type = types.nullOr types.str;
+    default = null;
+    description = mdDoc "URL of a synced beacon node to trust in obtaining checkpoint sync data. As an additional safety measure, it is strongly recommended to only use this option in conjunction with --weak-subjectivity-checkpoint flag";
+    example = "https://goerli.checkpoint-sync.ethpandaops.io";
+  };
+
+  genesis-beacon-api-url = mkOption {
+    type = types.nullOr types.str;
+    default = null;
+    description = mdDoc "URL of a synced beacon node to trust for obtaining genesis state. As an additional safety measure, it is strongly recommended to only use this option in conjunction with --weak-subjectivity-checkpoint flag";
+    example = "https://goerli.checkpoint-sync.ethpandaops.io";
+  };
+
+  p2p-udp-port = mkOption {
+    type = types.port;
+    default = 12000;
+    description = mdDoc "The port used by discv5.";
+  };
+
+  p2p-tcp-port = mkOption {
+    type = types.port;
+    default = 13000;
+    description = mdDoc "The port used by libp2p.";
+  };
+
+  disable-monitoring = mkOption {
+    type = types.bool;
+    default = false;
+    description = mdDoc "Disable monitoring service.";
+  };
+
+  monitoring-host = mkOption {
+    type = types.str;
+    default = "127.0.0.1";
+    description = mdDoc "Host used to listen and respond with metrics for prometheus.";
+  };
+
+  monitoring-port = mkOption {
+    type = types.port;
+    default = 8080;
+    description = mdDoc "Port used to listen and respond with metrics for prometheus.";
+  };
+
+  disable-grpc-gateway = mkOption {
+    type = types.bool;
+    default = false;
+    description = mdDoc "Disable the gRPC gateway for JSON-HTTP requests ";
+  };
+
+  grpc-gateway-host = mkOption {
+    type = types.str;
+    default = "127.0.0.1";
+    description = mdDoc "The host on which the gateway server runs on.";
+  };
+
+  grpc-gateway-port = mkOption {
+    type = types.port;
+    default = 3500;
+    description = mdDoc "The port on which the gateway server runs.";
+  };
+
+  rpc-host = mkOption {
+    type = types.str;
+    default = "127.0.0.1";
+    description = mdDoc "Host on which the RPC server should listen.";
+  };
+
+  rpc-port = mkOption {
+    type = types.port;
+    default = 4000;
+    description = mdDoc "RPC port exposed by a beacon node.";
+  };
+
+  pprof = mkOption {
+    type = types.bool;
+    default = false;
+    description = mdDoc "Enable the pprof HTTP server.";
+  };
+
+  pprofaddr = mkOption {
+    type = types.str;
+    default = "127.0.0.1";
+    description = mdDoc "pprof HTTP server listening interface.";
+  };
+
+  pprofport = mkOption {
+    type = types.port;
+    default = 6060;
+    description = mdDoc "pprof HTTP server listening port.";
+  };
+}
--- modules/prysm-beacon/default.nix
+++ modules/prysm-beacon/default.nix
@@ -16,160 +16,9 @@
   settingsFormat = pkgs.formats.yaml {};
 
   eachBeacon = config.services.ethereum.prysm-beacon;
-
-  beaconOpts = {
-    options = {
-      enable = mkEnableOption (mdDoc "Ethereum Beacon Chain Node from Prysmatic Labs");
-
-      args = {
-        network = mkOption {
-          type = types.nullOr (types.enum ["goerli" "prater" "ropsten" "sepolia"]);
-          default = null;
-          description = mdDoc "The network to connect to. Mainnet (null) is the default ethereum network.";
-        };
-
-        jwt-secret = mkOption {
-          type = types.str;
-          default = null;
-          description = mdDoc "Path to a file containing a hex-encoded string representing a 32 byte secret used for authentication with an execution node via HTTP";
-          example = "/var/run/prysm/jwtsecret";
-        };
-
-        checkpoint-sync-url = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = mdDoc "URL of a synced beacon node to trust in obtaining checkpoint sync data. As an additional safety measure, it is strongly recommended to only use this option in conjunction with --weak-subjectivity-checkpoint flag";
-          example = "https://goerli.checkpoint-sync.ethpandaops.io";
-        };
-
-        genesis-beacon-api-url = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = mdDoc "URL of a synced beacon node to trust for obtaining genesis state. As an additional safety measure, it is strongly recommended to only use this option in conjunction with --weak-subjectivity-checkpoint flag";
-          example = "https://goerli.checkpoint-sync.ethpandaops.io";
-        };
-
-        p2p-udp-port = mkOption {
-          type = types.port;
-          default = 12000;
-          description = mdDoc "The port used by discv5.";
-        };
-
-        p2p-tcp-port = mkOption {
-          type = types.port;
-          default = 13000;
-          description = mdDoc "The port used by libp2p.";
-        };
-
-        disable-monitoring = mkOption {
-          type = types.bool;
-          default = false;
-          description = mdDoc "Disable monitoring service.";
-        };
-
-        monitoring-host = mkOption {
-          type = types.str;
-          default = "127.0.0.1";
-          description = mdDoc "Host used to listen and respond with metrics for prometheus.";
-        };
-
-        monitoring-port = mkOption {
-          type = types.port;
-          default = 8080;
-          description = mdDoc "Port used to listen and respond with metrics for prometheus.";
-        };
-
-        disable-grpc-gateway = mkOption {
-          type = types.bool;
-          default = false;
-          description = mdDoc "Disable the gRPC gateway for JSON-HTTP requests ";
-        };
-
-        grpc-gateway-host = mkOption {
-          type = types.str;
-          default = "127.0.0.1";
-          description = mdDoc "The host on which the gateway server runs on.";
-        };
-
-        grpc-gateway-port = mkOption {
-          type = types.port;
-          default = 3500;
-          description = mdDoc "The port on which the gateway server runs.";
-        };
-
-        rpc-host = mkOption {
-          type = types.str;
-          default = "127.0.0.1";
-          description = mdDoc "Host on which the RPC server should listen.";
-        };
-
-        rpc-port = mkOption {
-          type = types.port;
-          default = 4000;
-          description = mdDoc "RPC port exposed by a beacon node.";
-        };
-
-        pprof = mkOption {
-          type = types.bool;
-          default = false;
-          description = mdDoc "Enable the pprof HTTP server.";
-        };
-
-        pprofaddr = mkOption {
-          type = types.str;
-          default = "127.0.0.1";
-          description = mdDoc "pprof HTTP server listening interface.";
-        };
-
-        pprofport = mkOption {
-          type = types.port;
-          default = 6060;
-          description = mdDoc "pprof HTTP server listening port.";
-        };
-      };
-
-      extraArgs = mkOption {
-        type = types.listOf types.str;
-        description = mdDoc "Additional arguments to pass to Prysm Beacon Chain.";
-        default = [];
-      };
-
-      package = mkOption {
-        default = pkgs.prysm;
-        defaultText = literalExpression "pkgs.prysm";
-        type = types.package;
-        description = mdDoc "Package to use for Prysm binary";
-      };
-
-      openFirewall = mkOption {
-        type = types.bool;
-        default = false;
-        description = lib.mdDoc "Open ports in the firewall for any enabled networking services";
-      };
-
-      # mixin backup options
-      backup = let
-        inherit (import ../backup/lib.nix lib) options;
-      in
-        options;
-
-      # mixin restore options
-      restore = let
-        inherit (import ../restore/lib.nix lib) options;
-      in
-        options;
-    };
-  };
 in {
   ###### interface
-
-  options = {
-    services.ethereum.prysm-beacon = mkOption {
-      type = types.attrsOf (types.submodule beaconOpts);
-      default = {};
-      description = mdDoc "Specification of one or more prysm beacon chain instances.";
-    };
-  };
+  inherit (import ./options.nix {inherit lib pkgs;}) options;
 
   ###### implementation
 
@@ -203,10 +52,12 @@ in {
           cfg: let
             scriptArgs = let
               # generate args
-              args = mkArgs {
-                inherit (cfg) args;
-                opts = beaconOpts.options.args;
-              };
+              args = let
+                opts = import ./args.nix lib;
+              in
+                mkArgs {
+                  inherit (cfg) args opts;
+                };
 
               # filter out certain args which need to be treated differently
               specialArgs = ["--network" "--jwt-secret"];
