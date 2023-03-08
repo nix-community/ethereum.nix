a73abc31b503f3a0e5ed7da779169a21364f7c3b
Brian McGee <brian@bmcgee.ie>
2023-03-08 17:33:21 +0000

    feat(docs): add initial mkdocs setup

--- docs/default.nix
+++ docs/default.nix
@@ -3,7 +3,40 @@
     lib,
     pkgs,
     ...
-  }: {
-    packages.docs-options = pkgs.callPackage ./options.nix {inherit lib;};
+  }: let
+    mkdocs = lib.getExe pkgs.mkdocs;
+    reference-doc = pkgs.callPackage ./reference.nix {inherit lib;};
+    src = ../.;
+  in {
+    packages.docs =
+      pkgs.runCommand "docs.html"
+      {
+        nativeBuildInputs = [pkgs.python310Packages.mkdocs-material];
+      } ''
+        mkdir $out
+        src=$(mktemp -d)
+        cp -R ${src}/* $src/
+        chmod -R u+w $src
+        cd $src
+        ls -al ./.
+        # link in options reference
+        ln -s ${reference-doc} ./docs/reference.md
+        ${mkdocs} build -d $out
+      '';
+
+    mission-control.scripts = with lib; {
+      serve-docs = {
+        category = "Docs";
+        description = "Serve the docs";
+        exec = ''
+          # link in options reference
+          rm -f ./docs/reference.md
+          ln -s ${reference-doc} ./docs/reference.md
+
+
+          ${mkdocs} serve
+        '';
+      };
+    };
   };
 }
