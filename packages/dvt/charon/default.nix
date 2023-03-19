8dea911b71687feeab98b52ea0924927f39466ae
Sergey Yakovlev <selfuryon@pm.me>
2023-03-19 13:53:45 +0200

    charon: 0.13.0 -> 0.14.2
    Diff: https://github.com/ObolNetwork/charon/compare/refs/tags/v0.13.0...v0.14.2

--- packages/dvt/charon/default.nix
+++ packages/dvt/charon/default.nix
@@ -6,16 +6,16 @@
 }:
 buildGoModule rec {
   pname = "charon";
-  version = "0.13.0";
+  version = "0.14.2";
 
   src = fetchFromGitHub {
     owner = "ObolNetwork";
     repo = "${pname}";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-j5Y+9RgJqWuTuVPIHSdTcAYVVb7G+06taBDVLbVqNaE=";
+    sha256 = "sha256-tgJ+iWyicudXwSjvR6hOa2Tkt8sJ6GqS4rPZb4AafMg=";
   };
 
-  vendorSha256 = "sha256-tz3fDnOJAEUbSeeRoSZ/2ha77bFYJKIwVx29vxACqYo=";
+  vendorSha256 = "sha256-ntXD4q6VkKtbs2iMz8u7QJL9g+N40ATdS74wvFmt33M=";
 
   buildInputs = [bls mcl];
 
