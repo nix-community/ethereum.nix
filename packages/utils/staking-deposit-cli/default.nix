120948d25ecc3bf540561af2518dc2fe5356cde0
Aldo <82811+aldoborrero@users.noreply.github.com>
2023-05-08 13:00:39 +0200

    feat: introduce staking-deposit-cli (#270)

---
+++ packages/utils/staking-deposit-cli/default.nix
@@ -0,0 +1,36 @@
+{
+  autoPatchelfHook,
+  fetchurl,
+  stdenv,
+  zlib,
+  ...
+}:
+stdenv.mkDerivation rec {
+  pname = "staking-deposit-cli";
+  version = "2.5.0";
+
+  src = fetchurl {
+    url = "https://github.com/ethereum/staking-deposit-cli/releases/download/v${version}/staking_deposit-cli-d7b5304-linux-amd64.tar.gz";
+    hash = "sha256-P1GFnXitR6PiWEcPWlyvA9Ge0dQwfVFzJbe7j2/N5u8=";
+  };
+
+  nativeBuildInputs = [autoPatchelfHook];
+
+  buildInputs = [zlib];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    mv ./deposit $out/bin/deposit
+
+    runHook postInstall
+  '';
+
+  meta = {
+    homepage = "https://github.com/ethereum/staking-deposit-cli";
+    description = "Secure key generation for deposits";
+    mainProgram = "deposit";
+    platforms = ["x86_64-linux"];
+  };
+}
