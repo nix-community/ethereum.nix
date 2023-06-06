6165c59747c0e43c352513d3efda0c2f17b92551
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-06-06 17:58:47 +0200

    feat: update network entries for mev-boost

--- modules/mev-boost/args.nix
+++ modules/mev-boost/args.nix
@@ -1,7 +1,7 @@
 lib:
 with lib; {
   network = mkOption {
-    type = types.nullOr (types.enum ["goerli" "sepolia"]);
+    type = types.nullOr (types.enum ["mainnet" "goerli" "sepolia" "zhejiang"]);
     default = null;
     description = mdDoc "The network to connect to. Mainnet (null) is the default ethereum network.";
   };
