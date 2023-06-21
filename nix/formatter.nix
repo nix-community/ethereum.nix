8b3f879090937905635dbb9e8d3c1f32293866a2
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-06-21 19:41:39 +0200

    feat: introduce mdformat-tables, mdformat-gfm, mdformat-admon plugins to mdformat

--- nix/formatter.nix
+++ nix/formatter.nix
@@ -7,7 +7,78 @@
     config,
     pkgs,
     ...
-  }: {
+  }: let
+    # TODO: Upstream this to nixpkgs
+    mdformat-tables = pkgs.python3Packages.buildPythonPackage rec {
+      pname = "mdformat-tables";
+      version = "0.4.1";
+
+      format = "flit";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "executablebooks";
+        repo = pname;
+        rev = "v${version}";
+        hash = "sha256-Q61GmaRxjxJh9GjyR8QCZOH0njFUtAWihZ9lFQJ2nQQ=";
+      };
+
+      buildInputs = with pkgs.python3Packages; [
+        mdformat
+      ];
+
+      doCheck = false;
+    };
+
+    # TODO: Upstream this to nixpkgs
+    mdformat-gfm = pkgs.python3Packages.buildPythonPackage rec {
+      pname = "mdformat-gfm";
+      version = "0.3.5";
+
+      format = "other";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "hukkin";
+        repo = pname;
+        rev = "refs/tags/${version}";
+        hash = "sha256-7sIa50jCN+M36Y0C05QaAL+TVwLzKxJ0gzpZI1YQFxg=";
+      };
+
+      buildInputs = with pkgs.python3Packages; [
+        mdformat
+        markdown-it-py
+        mdit-py-plugins
+        mdformat-tables
+      ];
+
+      doCheck = false;
+    };
+
+    # TODO: Upstream this to nixpkgs
+    mdformat-admon = pkgs.python3Packages.buildPythonPackage rec {
+      pname = "mdformat-admon";
+      version = "1.0.2";
+
+      format = "other";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "KyleKing";
+        repo = pname;
+        rev = "v${version}";
+        hash = "sha256-33Q3Re/axnoOHZ9XYA32mmK+efsSelJXW8sD7C1M/jU=";
+      };
+
+      buildInputs = with pkgs.python3Packages; [
+        mdformat
+        mdit-py-plugins
+      ];
+
+      doCheck = false;
+    };
+
+    mdformat-custom = pkgs.python3Packages.mdformat.overrideAttrs (_final: prev: {
+      propagatedBuildInputs = prev.propagatedBuildInputs ++ [mdformat-gfm mdformat-admon];
+    });
+  in {
     treefmt.config = {
       inherit (config.flake-root) projectRootFile;
       package = pkgs.treefmt;
@@ -17,6 +88,7 @@
         deadnix.enable = true;
         prettier.enable = true;
         mdformat.enable = true;
+        mdformat.package = mdformat-custom;
       };
       settings.formatter.prettier.excludes = ["*.md"];
     };
