5312c267b0612bb89cd0bdd794c33e2131dc81ee
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-06-21 16:52:06 +0200

    feat: support for mdformat to replace faulty formatting settings from prettier

--- nix/formatter.nix
+++ nix/formatter.nix
@@ -11,12 +11,14 @@
     treefmt.config = {
       inherit (config.flake-root) projectRootFile;
       package = pkgs.treefmt;
-
+      flakeFormatter = true;
       programs = {
         alejandra.enable = true;
         deadnix.enable = true;
         prettier.enable = true;
+        mdformat.enable = true;
       };
+      settings.formatter.prettier.excludes = ["*.md"];
     };
 
     devshells.default.commands = [
@@ -27,7 +29,5 @@
         command = "nix fmt";
       }
     ];
-
-    formatter = config.treefmt.build.wrapper;
   };
 }
