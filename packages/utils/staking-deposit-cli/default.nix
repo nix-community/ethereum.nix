87ee141f90da3530ea0ef4d0f86050585978e923
Sergey Yakovlev <selfuryon@pm.me>
2023-09-03 18:31:12 +0300

    chore(staking-deposit-cli): update to v2.6.0

--- packages/utils/staking-deposit-cli/default.nix
+++ packages/utils/staking-deposit-cli/default.nix
@@ -7,11 +7,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "staking-deposit-cli";
-  version = "2.5.0";
+  version = "2.6.0";
 
   src = fetchurl {
-    url = "https://github.com/ethereum/staking-deposit-cli/releases/download/v${version}/staking_deposit-cli-d7b5304-linux-amd64.tar.gz";
-    hash = "sha256-P1GFnXitR6PiWEcPWlyvA9Ge0dQwfVFzJbe7j2/N5u8=";
+    url = "https://github.com/ethereum/staking-deposit-cli/releases/download/v${version}/staking_deposit-cli-33cdafe-linux-amd64.tar.gz";
+    hash = "sha256-SHug2OJwD6Na3WzvajH6mkHNpDM9n4E2WG2DHTGrd6M=";
   };
 
   nativeBuildInputs = [autoPatchelfHook];
