b35585b70a25e4837c185d92ce8427067b46af94
Sergey Yakovlev <selfuryon@pm.me>
2023-09-03 18:33:10 +0300

    ethdo: 1.33.0 -> 1.33.1
    Diff: https://github.com/wealdtech/ethdo/compare/v1.33.0...v1.33.1

--- packages/utils/ethdo/default.nix
+++ packages/utils/ethdo/default.nix
@@ -9,17 +9,17 @@
 }:
 buildGoModule rec {
   pname = "ethdo";
-  version = "1.33.0";
+  version = "1.33.1";
 
   src = fetchFromGitHub {
     owner = "wealdtech";
     repo = "ethdo";
     rev = "v${version}";
-    hash = "sha256-h+pkdzyTQWaVHkipfDf+tDJO5Cq/2sv1iJknaV0aKmQ=";
+    hash = "sha256-rWQickCsQo8g4x5oW0SMyTYQqUPq4cXmNC4vg26ia3U=";
   };
 
   runVend = true;
-  vendorHash = "sha256-AhaOe8OxudnE8RSgsoN08dRVyue0UrCOPwL+7x63jqE=";
+  vendorHash = "sha256-/E4O09olBrzf6QrLegTz3+zgipmJ6FSVWG/Xn1T91oY=";
 
   nativeBuildInputs = [clang];
   buildInputs = [mcl bls];
