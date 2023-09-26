cefd1c9bf9c52bc0eac22333cf7ff4263c7512b7
Sergey Yakovlev <selfuryon@pm.me>
2023-09-27 00:34:25 +0300

    chore(reth): update to v0.1.0-alpha.10

--- packages/clients/execution/reth/default.nix
+++ packages/clients/execution/reth/default.nix
@@ -7,24 +7,22 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "reth";
-  version = "0.1.0-alpha.8";
+  version = "0.1.0-alpha.10";
 
   src = fetchFromGitHub {
     owner = "paradigmxyz";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-fQY2tW8pzFjA5UXwC71On4MKffd/ERYjhHIxNd+JWFQ=";
+    hash = "sha256-koGy06J2sBDY8eW0wG6ikhXEV4OXV1VuAkomIffHeUI=";
   };
 
   cargoLock = {
     lockFile = "${src}/Cargo.lock";
     outputHashes = {
-      "bindgen-0.64.0" = "sha256-api933laMWajI40cfSDbZkMp1zIagcl2lW1sw+kKv6Y=";
-      "boa_ast-0.17.0" = "sha256-56QzQF4BRuRx7ZzJXeYsjdkrKaoOFWJyYjQQwdp5gaE=";
-      "c-kzg-0.1.0" = "sha256-qj8S4zaH42nO2wS6K6ME9wgd4zmUWareQ+ABHyTifLw=";
+      "c-kzg-0.1.0" = "sha256-a0ZkshWGzChNibsKBBq8HhOtjRYgAuNlSTinZoAFDkY=";
       "discv5-0.3.1" = "sha256-Yl7cNCoBeFh+zBhAiziw0CDFHzoxpL6cVPUzZHpaEqM=";
       "igd-0.12.0" = "sha256-wjk/VIddbuoNFljasH5zsHa2JWiOuSW4VlcUS+ed5YY=";
-      "revm-3.3.0" = "sha256-jmDzHpbWTXxkv+ATAqYznvcQy8V3EF2XVsCyLaH4p0o=";
+      "revm-3.3.0" = "sha256-bljQRisFvnGwzPqvYmJIoEQ0hS/veD2oLfzqxcOq/qE=";
     };
   };
 
