2190ba57dfbf0a74038ad01df28f3ba8405cd782
Sergey Yakovlev <selfuryon@pm.me>
2023-11-08 20:01:21 +0200

    charon: 0.17.1 -> 0.17.2
    Diff: https://github.com/ObolNetwork/charon/compare/refs/tags/v0.17.1...v0.17.2

--- packages/dvt/charon/default.nix
+++ packages/dvt/charon/default.nix
@@ -6,13 +6,13 @@
 }:
 buildGoModule rec {
   pname = "charon";
-  version = "0.17.1";
+  version = "0.17.2";
 
   src = fetchFromGitHub {
     owner = "ObolNetwork";
     repo = "${pname}";
     rev = "refs/tags/v${version}";
-    hash = "sha256-v9nqm1LCehuAZ0S3BDo4yCfRsfxReu37s082nm4KbMA=";
+    hash = "sha256-+qqGFoBR2tFIs1ps98/WzM5RqI+E3i1B0g0+TKTzz7E=";
   };
 
   vendorHash = "sha256-SOsnjPvIJ2dqrWJD7dW/QZBkQZW2vdzTOALFCLGb2Ys=";
