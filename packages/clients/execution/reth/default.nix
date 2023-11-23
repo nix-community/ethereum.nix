63ec71e5535b652d28ea35cbc2b0879e59b6b77e
Sergey Yakovlev <selfuryon@pm.me>
2023-11-23 23:38:43 +0200

    chore(reth): update reth to 0.1.0-alpha.11

--- packages/clients/execution/reth/default.nix
+++ packages/clients/execution/reth/default.nix
@@ -7,22 +7,30 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "reth";
-  version = "0.1.0-alpha.10";
+  version = "0.1.0-alpha.11";
 
   src = fetchFromGitHub {
     owner = "paradigmxyz";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-koGy06J2sBDY8eW0wG6ikhXEV4OXV1VuAkomIffHeUI=";
+    hash = "sha256-llvaN9MNuLuOz5KumKKvRv4yiCfmpnLInNuS9DD48xg=";
   };
 
   cargoHash = "";
   cargoLock = {
     lockFile = "${src}/Cargo.lock";
-    allowBuiltinFetchGit = true;
+    outputHashes = {
+      "discv5-0.3.1" = "sha256-Z/Yl/K6UKmXQ4e0anAJZffV9PmWdBg/ROnNBrB8dABE=";
+      "igd-0.12.0" = "sha256-wjk/VIddbuoNFljasH5zsHa2JWiOuSW4VlcUS+ed5YY=";
+      "revm-3.5.0" = "sha256-IHKPHvcUqOp4wilygrweadbzMXrNP74J+bpIirJYZyc=";
+    };
   };
 
   nativeBuildInputs = [clang rustPlatform.bindgenHook];
+
+  checkFlags = [
+    "--skip=cli::tests::override_trusted_setup_file"
+  ];
 
   # Needed by libmdx
   LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
