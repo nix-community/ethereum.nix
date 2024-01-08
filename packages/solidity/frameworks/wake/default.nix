450f8e4694cc0806a481c26d7b35afb1fdeeb697
Sergey Yakovlev <selfuryon@pm.me>
2024-01-08 02:13:55 +0200

    chore: update wake package

--- packages/solidity/frameworks/wake/default.nix
+++ packages/solidity/frameworks/wake/default.nix
@@ -1,15 +1,14 @@
 {
   fetchFromGitHub,
   poetry2nix,
-  zlib,
   lib,
 }:
 poetry2nix.mkPoetryApplication {
   projectDir = fetchFromGitHub {
     owner = "Ackee-Blockchain";
-    repo = "woke";
-    rev = "v3.6.1";
-    sha256 = "sha256-dG2sPAUdl4TVeXe2c7TmbtRnJmqG65oqQYGV8ghc3yI=";
+    repo = "wake";
+    rev = "v4.3.0";
+    sha256 = "sha256-j//h63F8j6q0LL56EQoy9RcPMVfhmXp+VyDEJOqHfLg=";
   };
 
   # TODO: For now we install the basic version, including the *
@@ -17,46 +16,12 @@ poetry2nix.mkPoetryApplication {
   extras = [];
 
   overrides = poetry2nix.overrides.withDefaults (_self: super: {
-    eth-hash = super.eth-hash.overridePythonAttrs (old: {
-      buildInputs = (old.buildInputs or []) ++ [super.setuptools];
-    });
     abch-tree-sitter-solidity =
       super.abch-tree-sitter-solidity.overridePythonAttrs
-      (
-        old: {
-          buildInputs = (old.buildInputs or []) ++ [super.setuptools];
-        }
-      );
+      (old: {buildInputs = (old.buildInputs or []) ++ [super.setuptools];});
     abch-tree-sitter =
       super.abch-tree-sitter.overridePythonAttrs
-      (
-        old: {
-          buildInputs = (old.buildInputs or []) ++ [super.setuptools];
-        }
-      );
-    rich-click =
-      super.rich-click.overridePythonAttrs
-      (
-        old: {
-          buildInputs = (old.buildInputs or []) ++ [super.setuptools];
-        }
-      );
-    griffe =
-      super.griffe.overridePythonAttrs
-      (
-        old: {
-          buildInputs = (old.buildInputs or []) ++ [super.pdm-backend];
-        }
-      );
-    pillow =
-      super.pillow.overridePythonAttrs
-      (
-        _old: {
-          nativeBuildInputs = [
-            zlib
-          ];
-        }
-      );
+      (old: {buildInputs = (old.buildInputs or []) ++ [super.setuptools];});
     pywin32 = null;
   });
 
@@ -64,7 +29,7 @@ poetry2nix.mkPoetryApplication {
     homepage = "https://github.com/Ackee-Blockchain/wake";
     description = "Wake is a Python-based Solidity development and testing framework with built-in vulnerability detectors";
     changelog = "https://github.com/Ackee-Blockchain/wake/releases/tag/v${version}";
-    mainProgram = "woke";
+    mainProgram = "wake";
     license = licenses.mit;
     platforms = [
       "x86_64-linux"
