33d4d3da7fb61b2f5df8dd172e641edc1f304833
zimbatm <zimbatm@zimbatm.com>
2024-01-23 16:18:01 +0100

    geth: 1.13.5 -> 1.13.10

--- packages/clients/execution/geth/default.nix
+++ packages/clients/execution/geth/default.nix
@@ -13,23 +13,23 @@
     "devp2p"
     "ethkey"
     "evm"
-    "faucet"
     "geth"
+    "p2psim"
     "rlpdump"
   ];
 in
   buildGoModule rec {
     pname = "geth";
-    version = "1.13.5";
+    version = "1.13.10";
 
     src = fetchFromGitHub {
       owner = "ethereum";
       repo = "go-ethereum";
       rev = "v${version}";
-      hash = "sha256-UbRsY9fSUYAwPcLfGGDHeqvSsLKUKR+2a93jH5xA9uQ=";
+      hash = "sha256-HFAf5fjYiRx6OEbqdtgaan8bqZPHazYKWG2Ty+WhNu8=";
     };
 
-    vendorHash = "sha256-dOvpOCMxxmcAaticSLVlro1L4crAVJWyvgx/JZZ7buE=";
+    vendorHash = "sha256-w4WUDLzpr0VTwLK84/Yq4ia3Q1XhxDttzQ5RGg2FQGU=";
 
     ldflags = ["-s" "-w"];
 
@@ -50,8 +50,8 @@ in
       "cmd/devp2p"
       "cmd/ethkey"
       "cmd/evm"
-      "cmd/faucet"
       "cmd/geth"
+      "cmd/p2psim"
       "cmd/rlpdump"
       "cmd/utils"
     ];
