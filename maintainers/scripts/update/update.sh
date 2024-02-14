fd25852b979b89a99b7cc6d80ccb4017adf25459
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2024-02-14 20:16:16 +0100

    fix: pass arguments as argstr instead

--- maintainers/scripts/update/update.sh
+++ maintainers/scripts/update/update.sh
@@ -7,7 +7,6 @@ updateScript="maintainers/scripts/update/update.nix"
 
 nix-shell "${rootDir}/${updateScript}" \
   --argstr flakePath "${rootDir}" \
-  --arg keep-going 'true' \
-  --arg commit "true" \
-  --arg max-workers "4" \
-  --arg no-confirm "true"
+  --argstr keep-going 'true' \
+  --argstr max-workers "4" \
+  --argstr no-confirm "true"
