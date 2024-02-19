779ab65e984326152244b3b281060e8f450d139f
zimbatm <zimbatm@zimbatm.com>
2024-02-19 12:24:16 +0100

    vouch: remove duplication
    If we want to maintain multiple versions of packages, how do we reduce
    the duplication of definitions?

--- pkgs/vouch/unstable.nix
+++ pkgs/vouch/unstable.nix
@@ -1,31 +1,20 @@
 {
-  buildGoModule,
+  callPackage,
   fetchFromGitHub,
   mcl,
   bls,
-}:
-buildGoModule rec {
-  pname = "vouch";
-  version = "1.8.0-beta.3";
+}: let
+  mkVouch = callPackage ./builder.nix {inherit mcl bls;};
+in
+  mkVouch rec {
+    version = "1.8.0-beta.3";
 
-  src = fetchFromGitHub {
-    owner = "attestantio";
-    repo = pname;
-    rev = "v${version}";
-    hash = "sha256-CK6cLXJYMQL+ZRv8bYabgQ0GeDWK8Jcek6d/Wow2LI0=";
-  };
+    src = fetchFromGitHub {
+      owner = "attestantio";
+      repo = "vouch";
+      rev = "v${version}";
+      hash = "sha256-CK6cLXJYMQL+ZRv8bYabgQ0GeDWK8Jcek6d/Wow2LI0=";
+    };
 
-  runVend = true;
-  vendorHash = "sha256-Mp+RA2L4mOe089ceyKh3m5Q8zLsdwyx6geMFrqbn5Dk=";
-
-  buildInputs = [mcl bls];
-
-  doCheck = false;
-
-  meta = {
-    description = "An Ethereum 2 multi-node validator client";
-    homepage = "https://github.com/attestantio/vouch";
-    mainProgram = "vouch";
-    platforms = ["x86_64-linux"];
-  };
-}
+    vendorHash = "sha256-Mp+RA2L4mOe089ceyKh3m5Q8zLsdwyx6geMFrqbn5Dk=";
+  } {}
