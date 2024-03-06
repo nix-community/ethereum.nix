8783b5d3c9091e4048eefceb8b4f256a5cb9a733
Sergey Yakovlev <selfuryon@pm.me>
2024-03-06 03:04:04 +0200

    ssvnode: 1.3.0 -> 1.3.1
    Diff: https://github.com/bloxapp/ssv/compare/v1.3.0...v1.3.1

--- pkgs/ssvnode/default.nix
+++ pkgs/ssvnode/default.nix
@@ -6,16 +6,16 @@
 }:
 buildGo120Module rec {
   pname = "ssv";
-  version = "1.3.0";
+  version = "1.3.1";
 
   src = fetchFromGitHub {
     owner = "bloxapp";
     repo = "${pname}";
     rev = "v${version}";
-    hash = "sha256-mEkr35yhwWWZGICRsmIAKe6SoyddP3GmFgJXt1ioNp4=";
+    hash = "sha256-opd9o4ntLT047rarRQxNxvZp/4vNGAgSbuLhf4ZUBTo=";
   };
 
-  vendorHash = "sha256-AoJowh9HFjnMcM0RC0MeBeZy1c4bT2Nf8kF0CuSt73Q=";
+  vendorHash = "sha256-angs1oykJWDjkBZSsWnt+JcyhZIDPbSgkQ3TD/CzXQA=";
 
   buildInputs = [bls mcl];
 
