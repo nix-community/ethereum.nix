9c2d9726ed9fa29edebc6223d5ed1be6b5a6715d
Sebastian Bolaños <git@sebas.tech>
2024-08-08 18:44:50 +0200

    lighthouse: 5.2.1 -> 5.3.0

--- pkgs/lighthouse/default.nix
+++ pkgs/lighthouse/default.nix
@@ -27,13 +27,13 @@
 in
   rustPlatform.buildRustPackage rec {
     pname = "lighthouse";
-    version = "5.2.1";
+    version = "5.3.0";
 
     src = fetchFromGitHub {
       owner = "sigp";
       repo = pname;
       rev = "v${version}";
-      hash = "sha256-nItMiFPP4jeRsRwR8AJTyT5VCeIKths9UNgCZXNWqYE=";
+      hash = "sha256-wIj+YabyUrgLjWCfjCAH/Xb8jUG6ss+5SwnE2M82a+4";
     };
 
     patches = [
@@ -47,8 +47,8 @@ in
     cargoLock = {
       lockFile = ./Cargo.lock;
       outputHashes = {
-        "alloy-consensus-0.1.0" = "sha256-y5AIZN4d7Vm2dVa3jd0e6zXwC8hzPyOv0h5+W/Az3rs=";
-        "libmdbx-0.1.4" = "sha256-NMsR/Wl1JIj+YFPyeMMkrJFfoS07iEAKEQawO89a+/Q=";
+        "quick-protobuf-0.8.1" = "sha256-dgePLYCeoEZz5DGaLifhf3gEIPaL7XB0QT9wRKY8LJg=";
+        "libmdbx-0.1.4" = "sha256-ONp4uPkVCN84MObjXorCZuSjnM6uFSMXK1vdJiX074o=";
         "lmdb-rkv-0.14.0" = "sha256-sxmguwqqcyOlfXOZogVz1OLxfJPo+Q0+UjkROkbbOCk=";
       };
     };
