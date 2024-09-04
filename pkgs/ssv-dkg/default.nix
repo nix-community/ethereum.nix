0f87d1c08995b66c349be6a29bf19c146fb12a62
Juuso Haavisto <juuso@ponkila.com>
2024-09-04 16:51:19 +0100

    ssv-dkg: init at 2.1.0 (#542)
    * ssv-dkg: init at 2.1.0
    
    * ssv-dkg: address build issue
    
    * ssv-dkg: fix runtime version number

---
+++ pkgs/ssv-dkg/default.nix
@@ -0,0 +1,34 @@
+{
+  bls,
+  buildGo120Module,
+  fetchFromGitHub,
+  lib,
+  mcl,
+}:
+buildGo120Module rec {
+  pname = "ssv-dkg";
+  version = "2.1.0";
+
+  src = fetchFromGitHub {
+    owner = "ssvlabs";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-PXqjLvX9ewYtrOb3TDeNfuMxHamS1b6fh61FLAV2srE=";
+  };
+
+  vendorHash = "sha256-NtZRe8ldkYU2TXZQMEJ0bZyH44ZOknNdZON3cZhMOmg=";
+
+  buildInputs = [bls mcl];
+
+  subPackages = ["cmd/ssv-dkg"];
+
+  ldflags = ["-X main.Version=v${version}"];
+
+  meta = with lib; {
+    description = "The ssv-dkg tool enable operators to participate in ceremonies to generate distributed validator keys for Ethereum stakers.";
+    homepage = "https://github.com/ssvlabs/ssv-dkg";
+    license = with licenses; [gpl3Plus];
+    mainProgram = "ssv-dkg";
+    platforms = ["x86_64-linux" "aarch64-darwin" "aarch64-linux"];
+  };
+}
