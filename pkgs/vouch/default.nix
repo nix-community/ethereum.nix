b3b62c62957cc00b9a0b8476102ac492836ea926
Félinàun Chapeau <137397504+catwith1hat@users.noreply.github.com>
2024-10-30 21:27:10 +0100

    vouch: 1.9.0 -> 1.9.2 (#555)

--- pkgs/vouch/default.nix
+++ pkgs/vouch/default.nix
@@ -6,16 +6,16 @@
 }:
 buildGoModule rec {
   pname = "vouch";
-  version = "1.9.0";
+  version = "1.9.2";
 
   src = fetchFromGitHub {
     owner = "attestantio";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-EPBMFff9NoGy8L+aNilVZ2oHo2QcgwLyPxhRZDY6QrY=";
+    hash = "sha256-1FH4H4iH5uc7nUYVlQdqwYylB9puNJ557LLgy3ovcRI=";
   };
 
-  vendorHash = "sha256-sRoLkvULqYtA76TqgoEQK7CS59oalGH5V0SXlvlHWGw=";
+  vendorHash = "sha256-h5MQ3v3YLKxyOK16wb/p8ND68P5LaugWiIXXG8mtXqE=";
 
   runVend = true;
 
