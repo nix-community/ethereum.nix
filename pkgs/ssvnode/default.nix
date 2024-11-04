69bd8dcde94f05bf2e98daeeb33fa1dc9e26b091
Juuso Haavisto <juuso@ponkila.com>
2024-11-04 10:24:57 +0000

    ssvnode: 1.3.8 -> 2.0.0 (#547)
    * ssvnode: 1.3.8 -> 2.0.0-unstable.0
    
    * 2.0.0-unstable.0 -> 2.0.0-unstable.2
    
    * ssvnode: 2.0.0-unstable.2 -> 2.0.0

--- pkgs/ssvnode/default.nix
+++ pkgs/ssvnode/default.nix
@@ -1,29 +1,43 @@
 {
   bls,
-  mcl,
-  buildGo120Module,
+  buildGoModule,
   fetchFromGitHub,
+  mcl,
+  openssl,
 }:
-buildGo120Module rec {
+buildGoModule rec {
   pname = "ssv";
-  version = "1.3.8";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
-    owner = "bloxapp";
+    owner = "ssvlabs";
     repo = "${pname}";
-    rev = "v${version}";
-    hash = "sha256-5JUaJwo8snUrw/Uhk23uiGr+YV4UogiyvLGXXPiYICY=";
+    rev = "e39f1d0ec67c47f6ba437d83367aba0d2b4d34dd";
+    hash = "sha256-2/o+FyfJcX/Av82O3DV3gSkLHDtJCoUw2+zpozyr628=";
   };
 
-  vendorHash = "sha256-paFwSCVQEEkZzd/QHGBfaPvDwSAXYxvS5Cq+N18QTIU=";
+  vendorHash = "sha256-lr62X/Fo97CzxKgwdNCv3OELa6yMSEQBOs2cG53rYJY=";
 
   buildInputs = [bls mcl];
+
+  ldflags = [
+    "-X main.Commit=${src.rev}"
+    "-X main.Version=v${version}"
+  ];
+
+  # Dynamic loading of openssl
+  # See: https://github.com/ssvlabs/ssv/blob/v2.0.0-unstable.0/operator/keys/rsa_linux.go#L30
+  postFixup = ''
+    patchelf \
+      --add-rpath ${openssl.out}/lib \
+      $out/bin/ssvnode
+  '';
 
   subPackages = ["cmd/ssvnode"];
 
   meta = {
     description = "Secret-Shared-Validator(SSV) for ethereum staking";
-    homepage = "https://github.com/bloxapp/ssv";
+    homepage = "https://github.com/ssvlabs/ssv";
     platforms = ["x86_64-linux"];
     mainProgram = "ssvnode";
   };
