fb5029fdc6294b86e038cf3bea275c174fab7524
Martin Nikov <mnikov12@gmail.com>
2025-02-19 19:50:38 +0200

    nimbus: 24.12.0 > 25.3.0

--- pkgs/nimbus/default.nix
+++ pkgs/nimbus/default.nix
@@ -1,20 +1,16 @@
 {
-  applyPatches,
   fetchFromGitHub,
   pkgs,
   targets ? ["nimbus_beacon_node" "nimbus_validator_client" "gnosis-build" "gnosis-vc-build"],
   stableSystems ? ["x86_64-linux" "aarch64-linux"],
 }: let
-  version = "24.12.0";
-  src = applyPatches {
-    src = fetchFromGitHub {
-      owner = "status-im";
-      repo = "nimbus-eth2";
-      rev = "v${version}";
-      hash = "sha256-DBvsnGr91a69eCj1hAeoVOpxas5rfaT36rIxWEmvIVg=";
-      fetchSubmodules = true;
-    };
-    patches = [./fix-hash.patch];
+  version = "25.3.0";
+  src = fetchFromGitHub {
+    owner = "status-im";
+    repo = "nimbus-eth2";
+    rev = "v${version}";
+    hash = "sha256-v81pFj8V9IczTqbl3l3+NEMfesdmdnV3kQEi6iYBES0=";
+    fetchSubmodules = true;
   };
 in
   import "${src}/nix" {inherit pkgs targets stableSystems;}
