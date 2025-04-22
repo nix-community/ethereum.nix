fa0d175d9d371df372bf4c7f62daa04630463313
Julie B. <no-reply@bbjubjub.fr>
2025-04-22 09:31:16 +0200

    nimbus: 24.12.0 -> 25.4.1
    Co-Authored-By: Martin Nikov <mnikov12@gmail.com>

--- pkgs/by-name/ni/nimbus/default.nix
+++ pkgs/by-name/ni/nimbus/default.nix
@@ -1,20 +1,16 @@
 {
-  applyPatches,
   fetchFromGitHub,
   pkgs,
   targets ? ["nimbus_beacon_node" "nimbus_validator_client" "gnosis-build" "gnosis-vc-build"],
   stableSystems ? ["x86_64-linux" "aarch64-linux"],
 }: let
-  version = "24.12.0";
-  src = applyPatches {
-    src = fetchFromGitHub {
-      owner = "status-im";
-      repo = "nimbus-eth2";
-      rev = "v${version}";
-      hash = "sha256-DBvsnGr91a69eCj1hAeoVOpxas5rfaT36rIxWEmvIVg=";
-      fetchSubmodules = true;
-    };
-    patches = [./fix-hash.patch];
+  version = "25.4.1";
+  src = fetchFromGitHub {
+    owner = "status-im";
+    repo = "nimbus-eth2";
+    rev = "v${version}";
+    hash = "sha256-iMfi6S+ABB/rX+NgtBTO1n8KBre7spKGf12t4dlK9gY=";
+    fetchSubmodules = true;
   };
 in
   import "${src}/nix" {inherit pkgs targets stableSystems;}
