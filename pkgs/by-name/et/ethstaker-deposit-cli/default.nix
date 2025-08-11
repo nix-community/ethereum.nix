6dca1f4df792dc44f13679a3b7348e82bdc2dbd1
Félinàun Chapeau <137397504+catwith1hat@users.noreply.github.com>
2025-08-11 08:12:47 +0200

    ethstaker-deposit-cli: Init at 1.2.2 (#629)

---
+++ pkgs/by-name/et/ethstaker-deposit-cli/default.nix
@@ -0,0 +1,38 @@
+{
+  autoPatchelfHook,
+  fetchurl,
+  nix-update-script,
+  stdenv,
+  zlib,
+}:
+stdenv.mkDerivation rec {
+  pname = "staking-deposit-cli";
+  version = "1.2.2";
+
+  src = fetchurl {
+    url = "https://github.com/eth-educators/ethstaker-deposit-cli/releases/download/v1.2.2/ethstaker_deposit-cli-b13dcb9-linux-amd64.tar.gz";
+    hash = "sha256-BK8/T9L9zPSuBgq95HY3YioxEU2fLlPmJyKmlKTVsgY=";
+  };
+
+  nativeBuildInputs = [autoPatchelfHook];
+
+  buildInputs = [zlib];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    mv ./deposit $out/bin/deposit
+
+    runHook postInstall
+  '';
+
+  passthru.updateScript = nix-update-script {};
+
+  meta = {
+    description = "Secure key generation for deposits (ethstaker fork)";
+    homepage = "https://github.com/ethstaker/ethstaker-deposit-cli/";
+    mainProgram = "deposit";
+    platforms = ["x86_64-linux"];
+  };
+}
