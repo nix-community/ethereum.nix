bda207ed1f20b9e77a030167f0c9c2fe2df54102
Sergey Yakovlev <selfuryon@pm.me>
2025-08-26 15:05:23 +0300

    chore(eth-validator-watcher): update and fix to latest nixpkgs

--- pkgs/by-name/et/eth-validator-watcher/default.nix
+++ pkgs/by-name/et/eth-validator-watcher/default.nix
@@ -6,28 +6,40 @@
 }:
 python3.pkgs.buildPythonPackage rec {
   pname = "eth-validator-watcher";
-  version = "0.7.1";
+  version = "1.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "kilnfi";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-OhrThxzyuBSSdN/MM4wOj0yebVa219uQDW+o0xtsgTg=";
+    hash = "sha256-qgaXlh1P++0nNhA2mDK4pb+m0tqndCpvNZdyuVvz6IQ=";
   };
 
   nativeBuildInputs = with python3.pkgs; [
     poetry-core
+    setuptools
+    pybind11
   ];
 
   propagatedBuildInputs = with python3.pkgs; [
     typer
     prometheus-client
     pydantic
+    pydantic-settings
+    pydantic-yaml
+    pyyaml
     requests
     slack-sdk
     tenacity
     more-itertools
+    cachetools
+  ];
+
+  # Skip dependency checks for test dependencies
+  pythonRemoveDeps = [
+    "pytest-timeout"
+    "vcrpy"
   ];
 
   passthru.updateScript = nix-update-script {};
