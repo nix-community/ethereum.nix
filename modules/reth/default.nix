b872779e07c2a102e81ba8b421cefb1078887204
Sergey Yakovlev <selfuryon@pm.me>
2026-01-16 21:31:37 +0200

    feat(reth): migrate to RFC 42 settings pattern
    - Replace args.nix with freeform settings submodule
    - Use lib.cli.toCommandLine instead of custom mkArgs
    - Support flat dotted keys (e.g., "http.addr" instead of http.addr)
    - Simplify firewall port detection from settings

--- modules/reth/default.nix
+++ modules/reth/default.nix
@@ -4,138 +4,89 @@
   pkgs,
   ...
 }: let
-  inherit (lib.lists) optionals findFirst;
-  inherit (lib.strings) hasPrefix;
-  inherit
-    (lib)
-    concatStringsSep
-    filterAttrs
-    flatten
-    mapAttrs'
-    mapAttrsToList
-    mkIf
-    mkMerge
-    nameValuePair
-    zipAttrsWith
-    ;
+  inherit (lib) mkIf mkMerge mapAttrs' nameValuePair;
+  inherit (lib) concatStringsSep filterAttrs mapAttrsToList flatten optionals elem;
+  inherit (lib.attrsets) zipAttrsWith;
 
   modulesLib = import ../lib.nix lib;
-  inherit (modulesLib) baseServiceConfig mkArgs dotPathReducer;
+  inherit (modulesLib) baseServiceConfig;
 
-  eachNode = config.services.ethereum.reth;
+  eachReth = config.services.ethereum.reth;
+
 in {
-  # Disable the service definition currently in nixpkgs
   disabledModules = ["services/blockchain/ethereum/reth.nix"];
 
-  ###### interface
   inherit (import ./options.nix {inherit lib pkgs;}) options;
 
-  ###### implementation
-
-  config = mkIf (eachNode != {}) {
-    # configure the firewall for each service
+  config = mkIf (eachReth != {}) {
     networking.firewall = let
-      openFirewall = filterAttrs (_: cfg: cfg.openFirewall) eachNode;
+      openFirewall = filterAttrs (_: cfg: cfg.openFirewall) eachReth;
       perService =
         mapAttrsToList
-        (
-          _: cfg:
-            with cfg.args; {
-              allowedTCPPorts =
-                [port authrpc.port]
-                ++ (optionals http.enable [http.port])
-                ++ (optionals ws.enable [ws.port])
-                ++ (optionals metrics.enable [metrics.port]);
-            }
-        )
+        (_: cfg: let
+          s = cfg.settings;
+        in {
+          allowedTCPPorts =
+            [(s.port or 30303) (s."authrpc.port" or 8551)]
+            ++ optionals (s.http or false) [(s."http.port" or 8545)]
+            ++ optionals (s.ws or false) [(s."ws.port" or 8546)];
+        })
         openFirewall;
     in
       zipAttrsWith (_name: flatten) perService;
 
-    # configure systemd to create the state directory with a subvolume
     systemd.tmpfiles.rules =
       map
       (name: "v /var/lib/private/reth-${name}")
-      (builtins.attrNames (filterAttrs (_: v: v.subVolume) eachNode));
+      (builtins.attrNames (filterAttrs (_: v: v.subVolume) eachReth));
 
-    # create a service for each instance
     systemd.services =
       mapAttrs'
       (
-        rethName: let
+        rethName: cfg: let
           serviceName = "reth-${rethName}";
+          s = cfg.settings;
+          datadir = s.datadir or "%S/${serviceName}";
+          jwtSecret = s."authrpc.jwtsecret" or null;
+
+          # Keys to skip (handled separately)
+          skipKeys = ["datadir" "authrpc.jwtsecret"];
+          normalSettings = filterAttrs (k: _: !elem k skipKeys) s;
+
+          # Standard lib.cli
+          cliArgs = lib.cli.toCommandLine (name: {
+            option = "--${name}";
+            sep = null;
+            explicitBool = false;
+          }) normalSettings;
+
+          allArgs =
+            ["--datadir" datadir]
+            ++ cliArgs
+            ++ optionals (jwtSecret != null) ["--authrpc.jwtsecret" "%d/jwtsecret"]
+            ++ cfg.extraArgs;
+
+          scriptArgs = concatStringsSep " \\\n  " allArgs;
         in
-          cfg: let
-            scriptArgs = let
-              args = mkArgs {
-                opts = import ./args.nix lib;
-                pathReducer = dotPathReducer;
-                args = builtins.removeAttrs cfg.args ["ws"];
-              };
+          nameValuePair serviceName (mkIf cfg.enable {
+            description = "Reth Ethereum node (${rethName})";
+            wantedBy = ["multi-user.target"];
+            after = ["network.target"];
 
-              wsArgs = mkArgs {
-                opts = import ./args.nix lib;
-                args = {
-                  ws = builtins.removeAttrs cfg.args.ws ["enable"];
-                };
-              };
-
-              # filter out certain args which need to be treated differently
-              specialArgs = [
-                "--datadir"
-                "--authrpc.jwtsecret"
-                "--http.enable"
-                "--metrics.enable"
-                "--metrics.addr"
-                "--metrics.port"
-              ];
-
-              isNormalArg = name: (findFirst (arg: hasPrefix arg name) null specialArgs) == null;
-              filteredArgs =
-                (builtins.filter isNormalArg args)
-                ++ (optionals cfg.args.http.enable ["--http"])
-                ++ (optionals cfg.args.ws.enable wsArgs)
-                ++ (optionals cfg.args.metrics.enable ["--metrics" "${cfg.args.metrics.addr}:${toString cfg.args.metrics.port}"]);
-
-              jwtSecret =
-                if cfg.args.authrpc.jwtsecret != null
-                then "--authrpc.jwtsecret %d/jwtsecret"
-                else "";
-
-              datadir =
-                if cfg.args.datadir != null
-                then "${cfg.args.datadir}"
-                else "%S/${serviceName}";
-            in ''
-              --log.file.directory ${datadir}/logs \
-              --datadir ${datadir} \
-              ${jwtSecret} \
-              ${concatStringsSep " \\\n" filteredArgs} \
-              ${lib.escapeShellArgs cfg.extraArgs}
-            '';
-          in
-            nameValuePair serviceName (mkIf cfg.enable {
-              description = "Reth Ethereum node (${rethName})";
-              wantedBy = ["multi-user.target"];
-              after = ["network.target"];
-
-              # create service config by merging with the base config
-              serviceConfig = mkMerge [
-                baseServiceConfig
-                {
-                  User = serviceName;
-                  StateDirectory = serviceName;
-                  ExecStart = "${cfg.package}/bin/reth node ${scriptArgs}";
-
-                  # Reth needs this system call for some reason
-                  SystemCallFilter = ["@system-service" "~@privileged" "mincore"];
-                }
-                (mkIf (cfg.args.authrpc.jwtsecret != null) {
-                  LoadCredential = ["jwtsecret:${cfg.args.authrpc.jwtsecret}"];
-                })
-              ];
-            })
+            serviceConfig = mkMerge [
+              baseServiceConfig
+              {
+                User = serviceName;
+                StateDirectory = serviceName;
+                ExecStart = "${cfg.package}/bin/reth node ${scriptArgs}";
+                SystemCallFilter = ["@system-service" "~@privileged" "mincore"];
+              }
+              (mkIf (jwtSecret != null) {
+                LoadCredential = ["jwtsecret:${jwtSecret}"];
+              })
+            ];
+          })
       )
-      eachNode;
+      eachReth;
   };
 }
--- modules/reth/options.nix
+++ modules/reth/options.nix
@@ -3,21 +3,11 @@
   pkgs,
   ...
 }: let
-  args = import ./args.nix lib;
+  inherit (lib) mkEnableOption mkOption types literalExpression;
 
-  rethOpts = with lib; {
+  rethOpts = {
     options = {
-      enable = mkEnableOption "Reth Ethereum Node.";
-
-      subVolume = mkEnableOption "Use a subvolume for the state directory if the underlying filesystem supports it e.g. btrfs";
-
-      inherit args;
-
-      extraArgs = mkOption {
-        type = types.listOf types.str;
-        description = "Additional arguments to pass to Reth.";
-        default = [];
-      };
+      enable = mkEnableOption "Reth Ethereum Node";
 
       package = mkOption {
         type = types.package;
@@ -26,18 +16,51 @@
         description = "Package to use as Reth node.";
       };
 
+      subVolume = mkEnableOption "Use a subvolume for the state directory if the underlying filesystem supports it e.g. btrfs";
+
       openFirewall = mkOption {
         type = types.bool;
         default = false;
-        description = "Open ports in the firewall for any enabled networking services";
+        description = "Open ports in the firewall for any enabled networking services.";
+      };
+
+      settings = mkOption {
+        type = types.submodule {
+          freeformType = types.attrsOf types.anything;
+        };
+        default = {};
+        description = ''
+          Reth configuration. Converted to CLI arguments.
+
+          Use flat dotted keys (e.g., "http.addr" not http.addr).
+          Use http = true for --http flag.
+
+          See https://reth.rs for available options.
+        '';
+        example = literalExpression ''
+          {
+            chain = "mainnet";
+            http = true;
+            "http.addr" = "0.0.0.0";
+            "http.api" = ["eth" "net" "web3"];
+            ws = true;
+            "authrpc.jwtsecret" = "/var/run/reth/jwtsecret";
+            metrics = "127.0.0.1:9001";
+          }
+        '';
+      };
+
+      extraArgs = mkOption {
+        type = types.listOf types.str;
+        default = [];
+        description = "Additional arguments to pass to Reth.";
       };
     };
   };
 in {
-  options.services.ethereum.reth = with lib;
-    mkOption {
-      type = types.attrsOf (types.submodule rethOpts);
-      default = {};
-      description = "Specification of one or more Reth instances.";
-    };
+  options.services.ethereum.reth = mkOption {
+    type = types.attrsOf (types.submodule rethOpts);
+    default = {};
+    description = "Specification of one or more Reth instances.";
+  };
 }
