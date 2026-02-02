83afc7be90869b808320b1358820c58d8815d933
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2026-02-02 10:32:12 +0000

    chore: ensure main devshell is lean and use ci devshell for ci

--- devshells/default.nix
+++ devshells/default.nix
@@ -1,16 +1,8 @@
 { pkgs }:
 pkgs.mkShellNoCC {
   packages = [
-    pkgs.bash
-    pkgs.coreutils
-    pkgs.curl
-    pkgs.gh
-    pkgs.gnugrep
-    pkgs.gnused
-    pkgs.jq
     pkgs.nix-prefetch-scripts
     pkgs.nix-update
-    pkgs.nodejs
   ];
 
   shellHook = ''
