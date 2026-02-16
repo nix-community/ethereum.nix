5a9e5691d774c9d53eb196652f13380bc40de241
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2026-02-16 11:07:59 +0000

    op-conductor: init at op-node/v1.16.6

---
+++ packages/op-conductor/package.nix
@@ -0,0 +1,59 @@
+{
+  buildGoModule,
+  fetchFromGitHub,
+  lib,
+  versionCheckHook,
+}:
+buildGoModule rec {
+  pname = "op-conductor";
+  version = "1.16.6";
+
+  src = fetchFromGitHub {
+    owner = "ethereum-optimism";
+    repo = "optimism";
+    rev = "op-node/v${version}";
+    hash = "sha256-UxpgDtQ6n/iQC4oDPnIlictUmTmUaAKl2RNMGxhYG8Q=";
+  };
+
+  sourceRoot = "${src.name}/op-conductor";
+
+  proxyVendor = true;
+  vendorHash = "sha256-JcRCpMsTgKoibfKG/0MgvNLyZIHwt01sXZiGSQ48GXs=";
+
+  subPackages = [ "cmd" ];
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.Version=v${version}"
+    "-X main.GitCommit="
+    "-X main.GitDate="
+  ];
+
+  doCheck = false;
+
+  # The binary is named 'cmd' after the package, rename to op-conductor
+  postInstall = ''
+    mv $out/bin/cmd $out/bin/op-conductor
+  '';
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+
+  passthru = {
+    category = "Optimism";
+    skipAutoUpdate = true;
+  };
+
+  meta = with lib; {
+    description = "Optimism sequencer conductor for high-availability setups";
+    homepage = "https://github.com/ethereum-optimism/optimism/tree/develop/op-conductor";
+    license = licenses.mit;
+    mainProgram = "op-conductor";
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    sourceProvenance = [ sourceTypes.fromSource ];
+  };
+}
