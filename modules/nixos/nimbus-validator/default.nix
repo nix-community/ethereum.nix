0ae969489e61debf72c629f6ea800dbd521aec00
Sergey Yakovlev <selfuryon@pm.me>
2026-07-21 15:51:44 +0300

    refactor(nimbus-validator): migrate to RFC 42 settings pattern
    Bring nimbus-validator in line with nimbus-beacon and the rest of the module
    set: replace the ad-hoc network/extraArgs interface with a freeform settings
    option rendered via lib.cli.toCommandLine using sep = "=" (Nimbus/confutils
    requires --key=value). Selects the gnosis binary for gnosis/chiado, opens
    metrics/keymanager ports when requested, and creates the keymanager token
    file when the keymanager API is enabled.

--- modules/nixos/nimbus-validator/default.nix
+++ modules/nixos/nimbus-validator/default.nix
@@ -5,54 +5,130 @@
   ...
 }:
 let
+  modulesLib = import ../../../lib/modules.nix lib;
+
+  inherit (lib.attrsets) zipAttrsWith;
   inherit (lib)
+    concatStringsSep
+    filterAttrs
+    flatten
+    mapAttrs
     mapAttrs'
+    mapAttrsToList
     mkIf
     mkMerge
     nameValuePair
+    optionals
+    elem
     ;
-
-  modulesLib = import ../../../lib/modules.nix lib;
+  inherit (builtins) isList;
   inherit (modulesLib) baseServiceConfig;
 
-  eachNimbusValidator = config.services.ethereum.nimbus-validator;
+  eachValidator = config.services.ethereum.nimbus-validator;
+
+  # Convert lists to comma-separated strings for CLI
+  processSettings = mapAttrs (_: v: if isList v then concatStringsSep "," v else v);
+
+  # Gnosis networks use a different binary
+  gnosisNetworks = [
+    "gnosis"
+    "chiado"
+  ];
 in
 {
   ###### interface
   inherit (import ./options.nix { inherit lib pkgs; }) options;
 
   ###### implementation
+  config = mkIf (eachValidator != { }) {
+    # configure the firewall for each service
+    networking.firewall =
+      let
+        openFirewall = filterAttrs (_: cfg: cfg.openFirewall) eachValidator;
+        perService = mapAttrsToList (
+          _: cfg:
+          let
+            s = cfg.settings;
+          in
+          {
+            allowedTCPPorts =
+              (optionals (s.metrics or false) [ (s.metrics-port or 8008) ])
+              ++ (optionals (s.keymanager or false) [ (s.keymanager-port or 5052) ]);
+          }
+        ) openFirewall;
+      in
+      zipAttrsWith (_name: flatten) perService;
 
-  config = mkIf (eachNimbusValidator != { }) {
     # create a service for each instance
     systemd.services = mapAttrs' (
-      nimbusValidatorName:
+      validatorName:
       let
-        serviceName = "nimbus-validator-${nimbusValidatorName}";
+        serviceName = "nimbus-validator-${validatorName}";
       in
       cfg:
       let
+        s = cfg.settings;
+        dataDir = s.data-dir or "%S/${serviceName}";
+        network = s.network or validatorName;
+        keymanagerTokenFile = s.keymanager-token-file or "api-token.txt";
+
+        # Select appropriate binary based on network
         bin =
-          let
-            bins.gnosis = "nimbus_validator_client_gnosis";
-            bins.chiado = "nimbus_validator_client_gnosis";
-          in
-          bins.${cfg.network} or "nimbus_validator_client";
+          if elem network gnosisNetworks then "nimbus_validator_client_gnosis" else "nimbus_validator_client";
+
+        # Keys that need special handling
+        skipKeys = [
+          "data-dir"
+          "keymanager-token-file"
+        ];
+        normalSettings = filterAttrs (k: _: !elem k skipKeys) s;
+
+        # Nimbus (confutils) only accepts --key=value, never "--key value": a
+        # space-separated value is treated as a positional argument and rejected
+        # ("nimbus_validator_client does not accept arguments"). Join with "=".
+        cliArgs = lib.cli.toCommandLine (name: {
+          option = "--${name}";
+          sep = "=";
+          explicitBool = false;
+        }) (processSettings normalSettings);
+
+        allArgs = [
+          "--data-dir=${dataDir}"
+        ]
+        ++ cliArgs
+        ++ (optionals (s.keymanager or false) [
+          "--keymanager-token-file=${dataDir}/${keymanagerTokenFile}"
+        ])
+        ++ cfg.extraArgs;
+
+        scriptArgs = concatStringsSep " \\\n  " allArgs;
       in
       nameValuePair serviceName (
         mkIf cfg.enable {
           after = [ "network.target" ];
           wantedBy = [ "multi-user.target" ];
-          description = "Nimbus Validator Client (${nimbusValidatorName})";
+          description = "Nimbus Validator Client (${validatorName})";
 
-          # create service config by merging with the base config
           serviceConfig = mkMerge [
             baseServiceConfig
             {
+              # Nimbus requires JIT compilation
+              MemoryDenyWriteExecute = false;
+
               User = if cfg.user != null then cfg.user else serviceName;
               StateDirectory = serviceName;
-              ExecStart = "${cfg.package}/bin/${bin} ${lib.escapeShellArgs cfg.extraArgs}";
-              MemoryDenyWriteExecute = "false";
+
+              # Create keymanager token file if it doesn't exist (only when the
+              # keymanager API is enabled).
+              ExecStartPre = lib.mkBefore (
+                optionals (s.keymanager or false) [
+                  ''
+                    ${pkgs.coreutils-full}/bin/cp --no-preserve=all --update=none \
+                    /proc/sys/kernel/random/uuid ${dataDir}/${keymanagerTokenFile}''
+                ]
+              );
+
+              ExecStart = "${cfg.package}/bin/${bin} ${scriptArgs}";
 
               # Used by doppelganger detection to signal we should NOT restart.
               # https://nimbus.guide/doppelganger-detection.html
@@ -61,6 +137,6 @@ in
           ];
         }
       )
-    ) eachNimbusValidator;
+    ) eachValidator;
   };
 }
--- modules/nixos/nimbus-validator/options.nix
+++ modules/nixos/nimbus-validator/options.nix
@@ -4,50 +4,72 @@
   ...
 }:
 let
-  nimbusValidatorOpts = with lib; {
+  inherit (lib)
+    mkEnableOption
+    mkOption
+    types
+    literalExpression
+    ;
+
+  validatorOpts = {
     options = {
-      enable = mkEnableOption "Nimbus Validator Client.";
+      enable = mkEnableOption "Nimbus validator client";
 
       package = mkOption {
         type = types.package;
         default = pkgs.nimbus;
         defaultText = literalExpression "pkgs.nimbus";
-        description = "Package to use as Nimbus Validator Client.";
+        description = "Package to use for Nimbus binary.";
       };
 
-      extraArgs = mkOption {
-        type = types.listOf types.str;
-        description = "Additional arguments to pass to Nimbus Validator Client.";
-        default = [ ];
+      openFirewall = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Open ports in the firewall for any enabled networking services.";
       };
 
       user = mkOption {
         type = types.nullOr types.str;
-        description = "Service user";
+        default = null;
+        description = "User to run the systemd service. Defaults to nimbus-validator-<name>.";
       };
 
-      network = mkOption {
-        type = types.enum [
-          "mainnet"
-          "prater"
-          "sepolia"
-          "holesky"
-          "gnosis"
-          "chiado"
-          "hoodi"
-        ];
-        default = "mainnet";
-        description = "The Eth2 network to join";
+      settings = mkOption {
+        type = types.submodule {
+          freeformType = types.attrsOf types.anything;
+        };
+        default = { };
+        description = ''
+          Nimbus validator client configuration options. These are converted to CLI arguments.
+          Use flat dashed keys that match CLI flag names (e.g., "beacon-node", "rest-port").
+        '';
+        example = literalExpression ''
+          {
+            network = "mainnet";
+            beacon-node = [ "http://127.0.0.1:5052" ];
+            metrics = true;
+            metrics-address = "127.0.0.1";
+            metrics-port = 8008;
+            keymanager = true;
+            keymanager-address = "127.0.0.1";
+            keymanager-port = 5053;
+            doppelganger-detection = true;
+          }
+        '';
+      };
+
+      extraArgs = mkOption {
+        type = types.listOf types.str;
+        description = "Additional arguments to pass to Nimbus validator client.";
+        default = [ ];
       };
     };
   };
 in
 {
-  options.services.ethereum.nimbus-validator =
-    with lib;
-    mkOption {
-      type = types.attrsOf (types.submodule nimbusValidatorOpts);
-      default = { };
-      description = "Specification of one or more Nimbus Validator Client instances.";
-    };
+  options.services.ethereum.nimbus-validator = mkOption {
+    type = types.attrsOf (types.submodule validatorOpts);
+    default = { };
+    description = "Specification of one or more Nimbus validator client instances.";
+  };
 }
